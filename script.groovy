bcd51f149b0a8a5fb7a8cfb5ff5b3fa9b02240a6
import-hammad <hammadmalik67000@gmail.com>
2026-04-11 19:04:46 +0500

    ok

--- script.groovy
+++ script.groovy
@@ -1,17 +1,19 @@
-def buildjar(){
+def buildjar() {
     echo "building the jar file"
     sh "mvn package"
 }
 
-def buildapp(){
-    echo "deploying the app"
-    withCredentials ([usernamePassword(credentialsID: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS")]){
+def buildapp() {
+    echo "building and pushing the app"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
         sh 'docker build -t piratehammad/nana_practice_jenkins_2:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'}}'
-        sh 'docker push piratehammad/nana_practice_jenkins_2:jma-2.0')])
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push piratehammad/nana_practice_jenkins_2:jma-2.0'
+    }
 }
 
-def deployapp(){
+def deployapp() {
     echo "deploying the application"
 }
+
 return this
